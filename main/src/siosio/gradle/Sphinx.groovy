2d4a44ad90e24e288e953c26e5fb64e513341030
siosio <siosio12@gmail.com>
2012-06-06 08:53:27 +0900

    メソッド呼出でもsphinx-build出来るようにした。
    こんな感じに・・・
    apply plugin:SphinxPlugin
    
    task hoge << {
      sphinxBuild {
        target 'singlehtml'
      }
    }

--- main/src/siosio/gradle/Sphinx.groovy
+++ main/src/siosio/gradle/Sphinx.groovy
@@ -1,49 +1,111 @@
 package siosio.gradle
 
 import org.gradle.api.DefaultTask
+import org.gradle.api.Plugin
+import org.gradle.api.Project
 import org.gradle.api.internal.file.FileResolver
 import org.gradle.api.tasks.TaskAction
 import org.gradle.process.internal.DefaultExecAction
 import org.gradle.process.internal.ExecAction
+import org.gradle.util.ConfigureUtil
 
 class Sphinx extends DefaultTask {
 
-  private ExecAction action;
-  private buildCommand = "sphinx-build"
-  private target = "html"
-  private documentRoot = "document"
-  private output = "build/document"
+  private SphinxBuildAction action
 
   Sphinx() {
-    action = new DefaultExecAction(getServices().get(FileResolver.class))
+    action = new SphinxBuildAction(getServices().get(FileResolver.class))
   }
 
   @TaskAction
   def build() {
     action.executable = buildCommand
     action.args = ['-b', target, documentRoot, output]
-    println "sphinx-build command line option:${action.args.join(' ')}"
-    def execResult = action.execute()
   }
 
   def setBuildCommand(def buildCommand) {
-    this.buildCommand = buildCommand
+    action.buildCommand = buildCommand
     this
   }
 
   def setTarget(def target) {
-    this.target = target
+    action.target = target
     this
   }
 
   def setDocumentRoot(def documentRoot) {
-    this.documentRoot = documentRoot
+    action.documentRoot = documentRoot
     this
   }
 
   def setOutput(def output) {
+    action.output = output
+    this
+  }
+
+  def setOptions(def options) {
+    action.options = options
+    this
+  }
+}
+
+class SphinxPlugin implements Plugin<Project> {
+
+  @Override
+  void apply(Project target) {
+    target.metaClass.sphinxBuild {closure ->
+      def action = new SphinxBuildAction(target.fileResolver)
+      ConfigureUtil.configure(closure, action)
+      action.execute()
+    }
+  }
+}
+class SphinxBuildAction {
+
+  def ExecAction action;
+  def buildCommand = "sphinx-build"
+  def target = "html"
+  def documentRoot = "document"
+  def output = "build/document"
+  def options = []
+
+  SphinxBuildAction(def fileResolver) {
+    action = new DefaultExecAction(fileResolver)
+  }
+
+  def execute() {
+    action.executable = buildCommand
+    def args = ['-b', target]
+    args += options
+    args += [documentRoot, output]
+    action.args = args
+    println "sphinx-build command line option:${action.args.join(' ')}"
+    action.execute()
+  }
+
+  def target(String target) {
+    this.target = target
+    this
+  }
+
+  def buildCommand(def buildCommand) {
+    this.buildCommand = buildCommand
+    this
+  }
+
+  def documentRoot(def documentRoot) {
+    this.documentRoot = documentRoot
+    this
+  }
+
+  def output(def output) {
     this.output = output
     this
   }
 
+  def options(def options) {
+    this.options = options
+    this
+  }
 }
+
